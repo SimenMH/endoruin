9da16357683abbc4409da33dca5ecb70f33948e5
Simen H <simenmelo@gmail.com>
2021-11-16 00:52:56 +0100

    Implement equipping and unequipping items

--- scenes/GUI/inventory/InvSlot.gd
+++ scenes/GUI/inventory/InvSlot.gd
@@ -8,6 +8,8 @@ func update_item(new_item):
 	if new_item != null:
 		var res = load('res://sprites/items/' + new_item.name + '.png')
 		$ItemIcon.texture = res
+	else:
+		$ItemIcon.texture = null
 
 func _process(delta):
 	if selected:
--- scenes/GUI/inventory/Inventory.gd
+++ scenes/GUI/inventory/Inventory.gd
@@ -23,9 +23,54 @@ func _process(delta):
 	if visible:
 		get_nav_input()
 	# Testing
-	if Input.is_action_just_pressed('ui_accept'):
-		add_to_inventory({'name': 'Shortsword'})
+	if Input.is_action_just_pressed('ui_select'):
+		add_to_inventory({'name': 'Shortsword', 'slot': 'mhand'})
 		update_inventory()
+
+func initialize_inventory():
+	for i in range(inventory_space):
+		inventory.append(null)
+
+func update_inventory():
+	for i in range(inventory.size()):
+		if i >= inventory_space:
+			print('ERROR: Inventory index out of range')
+			break
+		var slot_idx = i
+		if grow_vertically:
+			slot_idx = get_vert_slot_idx(i)
+		var slot = inventory_slots.get_child(slot_idx)
+		slot.update_item(inventory[i])
+
+func update_equipment():
+	var slots = equipment_slots.get_children()
+	for slot in slots:
+		var slot_type = slot.name.to_lower()
+		slot.get_child(1).update_item(equipment[slot_type])
+
+func get_vert_slot_idx(idx):
+	var row = idx - (idx / rows * rows)
+	var column = idx / rows
+	var slot_idx = (row * columns) + column
+	return slot_idx
+
+func get_vert_inv_idx(idx):
+	var row = idx / columns
+	var column = idx - (idx / columns * columns)
+	var inv_idx = (rows * column) + row
+	return inv_idx
+
+func add_to_inventory(item):
+	for i in range(inventory.size()):
+		if inventory[i] == null:
+			inventory[i] = item
+			return true
+	if inventory.size() < inventory_space:
+		inventory.append(item)
+		return true
+	else:
+		print('Inventory full!')
+		return false
 
 func get_nav_input():
 	var new_slot_idx
@@ -75,6 +120,8 @@ func get_nav_input():
 			new_slot_idx = selected_slot_idx + 1
 			if new_slot_idx % columns == 0:
 				new_slot_idx = - int(new_slot_idx / columns)
+	if Input.is_action_just_pressed('ui_accept'):
+		select_item()
 	update_selected_slot(new_slot_idx)
 
 func update_selected_slot(idx):
@@ -98,41 +145,28 @@ func update_selected_slot(idx):
 		
 		selected_slot_idx = idx
 
-func initialize_inventory():
-	for i in range(inventory_space):
-		inventory.append(null)
-
-func update_equipment():
-	var slots = equipment_slots.get_children()
-	for slot in slots:
-		var slot_type = slot.name.to_lower()
-		slot.get_child(1).update_item(equipment[slot_type])
-
-func update_inventory():
-	for i in range(inventory.size()):
-		if i >= inventory_space:
-			print('ERROR: Inventory index out of range')
-			break
-		var slot_idx = i
-		if grow_vertically:
-			slot_idx = get_vert_slot_idx(i)
-		var slot = inventory_slots.get_child(slot_idx)
-		slot.update_item(inventory[i])
-
-func get_vert_slot_idx(idx):
-	var column = idx / rows
-	var row = idx - (idx / rows * rows)
-	var slot_idx = (row * inventory_slots.columns) + column
-	return slot_idx
-
-func add_to_inventory(item):
-	for i in range(inventory.size()):
-		if inventory[i] == null:
-			inventory[i] = item
-			return true
-	if inventory.size() < inventory_space:
-		inventory.append(item)
-		return true
+func select_item():
+	if selected_slot_idx >= 0:
+		equip_item(selected_slot_idx)
 	else:
-		print('Inventory full!')
-		return false
+		unequip_item(selected_slot_idx)
+
+func equip_item(inv_idx):
+	if grow_vertically:
+		inv_idx = get_vert_inv_idx(inv_idx)
+	var item = inventory[inv_idx]
+	if item && item.has('slot'):
+		inventory[inv_idx] = equipment[item.slot]
+		equipment[item.slot] = item
+	update_inventory()
+	update_equipment()
+
+func unequip_item(equip_idx):
+	equip_idx = abs(equip_idx + 1)
+	var slot_type = equipment_slots.get_child(equip_idx).name.to_lower()
+	if equipment.has(slot_type):
+		var cur_equipped = equipment[slot_type]
+		if add_to_inventory(cur_equipped):
+			equipment[slot_type] = null
+	update_inventory()
+	update_equipment()
