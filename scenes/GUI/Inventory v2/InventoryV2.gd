09f3d473571d4c2ab78c3bd45bb90eeadd3d562c
Simen H <simenmelo@gmail.com>
2022-05-11 19:43:10 +0200

    Implement equipping items in Inventory v2

--- scenes/GUI/Inventory v2/InventoryV2.gd
+++ scenes/GUI/Inventory v2/InventoryV2.gd
@@ -17,8 +17,10 @@ var test_item = {
 	"description": "Lorem ipsum"
 }
 
+onready var inventory = PlayerData.inventory
+onready var equipment = PlayerData.equipment
 var inv_slots = []
-var inv_size = [0, 0]
+var inv_size = [0, 0, 0] # x, y, total
 var inv_pos = Vector2(1, 0)
 
 func _physics_process(delta):
@@ -31,17 +33,20 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed('ui_down'):
 		move_in_inv(Vector2.DOWN)
 	if Input.is_action_just_pressed('ui_select'):
-		add_to_inventory(test_item)
+		add_to_inventory2(test_item)
 	if Input.is_action_just_pressed('ui_accept'):
 		select_item()
 
 func _ready():
 	get_inv_size()
 	inv_slots[inv_pos.x][inv_pos.y].selected = true
+	update_equipment()
+	update_inventory()
 
 func get_inv_size():
-	inv_size[0] = $CenterContainer/HBoxContainer/EqInvSlotContainer.get_child_count()
+	inv_size[0] = $CenterContainer/HBoxContainer/InvSlotContainer.get_child_count()
 	inv_size[1] = $CenterContainer/HBoxContainer/InvSlotContainer/VBoxContainer.get_child_count()
+	inv_size[2] = inv_size[0] * inv_size[1]
 	
 	var inv_container = $CenterContainer/HBoxContainer/InvSlotContainer/
 	
@@ -98,16 +103,64 @@ func add_to_inventory(item):
 				return
 
 
+func add_to_inventory2(item):
+	for i in range(inventory.size()):
+		if inventory[i] == null:
+			inventory[i] = item
+			update_inventory()
+			return true
+	if inventory.size() < inv_size[2]:
+		inventory.append(item)
+		update_inventory()
+		return true
+	else:
+		print('Inventory full!')
+		return false
+
 func select_item():
 	var selected_slot = inv_slots[inv_pos.x][inv_pos.y]
 	var item = selected_slot.item
 	if item != null:
 		if inv_pos.x == 0:
 			# Equipment slot
-			add_to_inventory(item)
-			selected_slot.update_item(null)
-		else:
+			select_equipped_item()
+		elif item.has('type'):
+			var inv_idx = pos_to_index(inv_pos.x, inv_pos.y)
 			if item.type == 'weapon':
-				var weapon_slot = inv_slots[0][4]
-				selected_slot.update_item(weapon_slot.item)
-				weapon_slot.update_item(item)
+				inventory[inv_idx] = equipment['mhand']
+				equipment['mhand'] = item
+			else:
+				inventory[inv_idx] = equipment[item.type]
+				equipment[item.type] = item
+		update_equipment()
+		update_inventory()
+
+func select_equipped_item():
+	var selected_slot = inv_slots[inv_pos.x][inv_pos.y]
+	var slot_type = selected_slot.name.to_lower()
+	var cur_equipped = equipment[slot_type]
+	if (add_to_inventory2(cur_equipped)):
+		equipment[slot_type] = null
+	else:
+		print('No room in inventory to unequip item')
+
+func update_equipment():
+	var equipment_slots = inv_slots[0]
+	for slot in equipment_slots:
+		var slot_type = slot.name.to_lower()
+		slot.update_item(equipment[slot_type])
+	emit_signal('update_player_stats')
+
+func update_inventory():
+	for i in range(inventory.size()):
+		var pos = idx_to_pos(i)
+		var slot = inv_slots[pos.x][pos.y]
+		slot.update_item(inventory[i])
+
+func pos_to_index(x, y):
+	return (x - 1) * inv_size[1] + y
+
+func idx_to_pos(idx):
+	var x = floor(idx / inv_size[1]) + 1
+	var y = idx % inv_size[1]
+	return Vector2(x, y)
